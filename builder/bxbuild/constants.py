25e3d307cb27e5db2573e603e0eb8f67cbbdea1a
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2021-06-11 15:39:17 +0530

    📦 Source .png files

--- builder/bxbuild/constants.py
+++ builder/bxbuild/constants.py
@@ -18,52 +18,54 @@ X_CURSORS_CFG: Dict[str, Dict[str, int]] = {
     ##########
     # Static #
     ##########
-    "all-scroll.png": {"xhot": 100, "yhot": 100},
-    "bottom_left_corner.png": {"xhot": 98, "yhot": 96},
-    "bottom_right_corner.png": {"xhot": 99, "yhot": 96},
-    #
-    "bottom_tee.png": {"xhot": 100, "yhot": 100},
+    "bd_double_arrow.png": {"xhot": 100, "yhot": 100},
+    "bottom_left_corner.png": {"xhot": 100, "yhot": 100},
+    "bottom_right_corner.png": {"xhot": 100, "yhot": 100},
+    "bottom_side.png": {"xhot": 100, "yhot": 100},
+    "center_ptr.png": {"xhot": 100, "yhot": 100},
     "context-menu.png": {"xhot": 100, "yhot": 100},
     "copy.png": {"xhot": 100, "yhot": 100},
     "cross.png": {"xhot": 100, "yhot": 100},
+    "crossed_circle.png": {"xhot": 100, "yhot": 100},
     "dnd_no_drop.png": {"xhot": 100, "yhot": 100},
     "dotbox.png": {"xhot": 100, "yhot": 100},
+    "fd_double_arrow.png": {"xhot": 100, "yhot": 100},
     "hand1.png": {"xhot": 100, "yhot": 100},
     "hand2.png": {"xhot": 100, "yhot": 100},
     "left_ptr.png": {"xhot": 100, "yhot": 100},
-    "left_tee.png": {"xhot": 100, "yhot": 100},
+    "left_side.png": {"xhot": 100, "yhot": 100},
     "link.png": {"xhot": 100, "yhot": 100},
-    "ll_angle.png": {"xhot": 100, "yhot": 100},
-    "lr_angle.png": {"xhot": 100, "yhot": 100},
     "move.png": {"xhot": 100, "yhot": 100},
     "pencil.png": {"xhot": 100, "yhot": 100},
+    "pirate.png": {"xhot": 100, "yhot": 100},
     "plus.png": {"xhot": 100, "yhot": 100},
     "question_arrow.png": {"xhot": 100, "yhot": 100},
     "right_ptr.png": {"xhot": 100, "yhot": 100},
-    "right_tee.png": {"xhot": 100, "yhot": 100},
+    "right_side.png": {"xhot": 100, "yhot": 100},
     "sb_down_arrow.png": {"xhot": 100, "yhot": 100},
     "sb_h_double_arrow.png": {"xhot": 100, "yhot": 100},
     "sb_left_arrow.png": {"xhot": 100, "yhot": 100},
     "sb_right_arrow.png": {"xhot": 100, "yhot": 100},
     "sb_up_arrow.png": {"xhot": 100, "yhot": 100},
     "sb_v_double_arrow.png": {"xhot": 100, "yhot": 100},
-    "top_tee.png": {"xhot": 100, "yhot": 100},
-    "ul_angle.png": {"xhot": 100, "yhot": 100},
-    "ur_angle.png": {"xhot": 100, "yhot": 100},
+    "top_left_corner.png": {"xhot": 100, "yhot": 100},
+    "top_right_corner.png": {"xhot": 100, "yhot": 100},
+    "top_side.png": {"xhot": 100, "yhot": 100},
     "vertical-text.png": {"xhot": 100, "yhot": 100},
     "wayland-cursor.png": {"xhot": 100, "yhot": 100},
     "X_cursor.png": {"xhot": 100, "yhot": 100},
-    "xterm.png": {"xhot": 100, "yhot": 89},
+    "xterm.png": {"xhot": 100, "yhot": 100},
     "zoom-in.png": {"xhot": 100, "yhot": 100},
     "zoom-out.png": {"xhot": 100, "yhot": 100},
     ############
     # Animated #
     ############
-    # Note: Animated cursors don't need an extension and frame numbers.
+    # NOTE: Animated cursors don't need an extension and frame numbers.
     "left_ptr_watch": {"xhot": 100, "yhot": 100},
     "wait": {"xhot": 100, "yhot": 100},
 }
 
+# TODO setting windows cursors
 WIN_CURSORS_CFG: Dict[str, Dict[str, str]] = {
     ##########
     # Static #
@@ -84,7 +86,7 @@ WIN_CURSORS_CFG: Dict[str, Dict[str, str]] = {
     ############
     # Animated #
     ############
-    # Note: Animated cursors don't need frame numbers.
+    # NOTE: Animated cursors don't need frame numbers.
     "left_ptr_watch": {"to": "Work", "position": "top_left"},
     "wait": {"to": "Busy"},
 }
